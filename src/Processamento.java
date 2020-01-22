dcf33bf84295fe7174afa845094444bba4d83ca2
Pedro <p.paiva.pdo@gmail.com>
2020-01-22 17:41:29 -0300

    :hammer: Corrigido a conversao de string pra date com criacao de métodos

--- src/Processamento.java
+++ src/Processamento.java
@@ -1,9 +1,11 @@
 
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 import Layout.Detalhe;
 import Layout.Header;
@@ -21,12 +23,18 @@ public class Processamento {
 			FileReader arq = new FileReader(nArquivo);
 			BufferedReader bf = new BufferedReader(arq);
 			String registro = bf.readLine();
+			Date date;
 			while(registro != null) {
 				int tipoRegistro = verificaRegistro(registro);
-				
 				if(tipoRegistro == 0 ) {
 					headerArquivo.setTipoRegistro(Integer.parseInt(registro.substring(0,1)));
 					headerArquivo.setEstabelecimento(Long.parseLong(registro.substring(1,11)));
+					date = transformarStringDate(registro.substring(11,19));
+					headerArquivo.setDataProcessamento(date);
+					date = transformarStringDate(registro.substring(19,27));
+					headerArquivo.setPeriodoInicial(date);
+					date = transformarStringDate(registro.substring(27,35));
+					headerArquivo.setperiodoFinal(date);
 					headerArquivo.setSequencia(Integer.parseInt(registro.substring(35,42)));
 					headerArquivo.setEmpresaAdquirinte(registro.substring(42,47));
 					headerArquivo.setTipoExtrato(Integer.parseInt(registro.substring(47,49)));
@@ -66,12 +74,16 @@ public class Processamento {
 	
 	public Detalhe montarVenda(String registro) {
 		Detalhe venda = new Detalhe();
-		
+		Date date;
+		Date hour;
 		venda.setTipoRegistro(Integer.parseInt(registro.substring(0,1)));
 		venda.setEstabelecimento(Integer.parseInt(registro.substring(1,11)));
-		//venda.setDataInicialTransacao(Integer.parseInt(registro.substring(11,19)));
-		//venda.setDataEvento(Integer.parseInt(registro.substring(19,27)));
-		//venda.setHoraEvento(Integer.parseInt(registro.substring(27,33))); // vai ate o 33
+		date = transformarStringDate(registro.substring(11, 19));
+		venda.setDataInicialTransacao(date);
+		date = transformarStringDate(registro.substring(19, 27));
+		venda.setDataEvento(date);
+		hour = transformaStringHora(registro.substring(27, 33));
+		venda.setHoraEvento(hour); // vai ate o 33
 		venda.setTipoEvento(Integer.parseInt(registro.substring(33,35)));
 		venda.setTipoTransacao(Integer.parseInt(registro.substring(35,37)));
 		venda.setNumeroSerieLeitor(registro.substring(37,45));
@@ -83,7 +95,8 @@ public class Processamento {
 		venda.setPlano(registro.substring(124,126));
 		venda.setParcela(registro.substring(126,128));
 		venda.setQtdParcela(Integer.parseInt(registro.substring(128,130)));
-		//venda.setDataPrevistaPagamento(Integer.parseInt(registro.substring(130,138)));
+		date = transformaStringHora(registro.substring(130,138));
+		venda.setDataPrevistaPagamento(date);
 		venda.setTaxaParcelamentoComprador(Long.parseLong(registro.substring(138,151)));
 		venda.setTarifaBoleto(Long.parseLong(registro.substring(151,164)));
 		venda.setValorOriginalTransacao(Long.parseLong(registro.substring(164,177)));
@@ -113,6 +126,36 @@ public class Processamento {
 	
 	public Trailler getTrailler(){
 		return traillerArquivo;
+	}
+
+	public Date transformarStringDate(String d){
+		String date = "";
+		date = date+d.charAt(0)+d.charAt(1)+d.charAt(2)+d.charAt(3)+"/"+d.charAt(4)+d.charAt(5)+"/"+d.charAt(6)+d.charAt(7);
+		Date dataN = null;
+		try {
+			SimpleDateFormat formato = new SimpleDateFormat("yyyy/MM/dd"); 
+			dataN = formato.parse(date);
+			return dataN;
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Erro em converter a data");
+			return dataN;
+		}
+	}
+
+	public Date transformaStringHora(String d){
+		String date = "";
+		date = date+d.charAt(0)+d.charAt(1)+":"+d.charAt(2)+d.charAt(3)+":"+d.charAt(4)+d.charAt(5);
+		Date dataN = null;
+		try {
+			SimpleDateFormat formato = new SimpleDateFormat("HH:mm:ss"); 
+			dataN = formato.parse(date);
+			return dataN;
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Erro em converter a data");
+			return dataN;
+		}
 	}
 		
 }
