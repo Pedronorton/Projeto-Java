cdb8e9ca62a21333edcb49061141c58f36002ef5
Pedro <p.paiva.pdo@gmail.com>
2020-01-23 23:58:20 -0300

    :recycle: refatorado a instancia dos objetos Header, Detalhe e Trailler

--- src/Processamento.java
+++ src/Processamento.java
@@ -15,53 +15,34 @@ import Layout.Trailler;
 public class Processamento {
 	Trailler traillerArquivo;
 	Header headerArquivo;
-	
-	public Processamento(String nArquivo, ArrayList<Detalhe> listaVendas) {
-		 headerArquivo = new Header();
-		 traillerArquivo = new Trailler();
+	Detalhe venda;
+	public Processamento(String nomeArquivo, ArrayList<Detalhe> listaVendas) {
 		try {
-			FileReader arq = new FileReader(nArquivo);
-			BufferedReader bf = new BufferedReader(arq);
-			String registro = bf.readLine();
-			Date date;
+			FileReader arquivo = new FileReader(nomeArquivo);
+			BufferedReader bufferedReaderArquivo = new BufferedReader(arquivo);
+			String registro = bufferedReaderArquivo.readLine();
+
 			while(registro != null) {
 				int tipoRegistro = verificaRegistro(registro);
 				if(tipoRegistro == 0 ) {
-					headerArquivo.setTipoRegistro(Integer.parseInt(registro.substring(0,1)));
-					headerArquivo.setEstabelecimento(Long.parseLong(registro.substring(1,11)));
-					date = transformarStringDate(registro.substring(11,19));
-					headerArquivo.setDataProcessamento(date);
-					date = transformarStringDate(registro.substring(19,27));
-					headerArquivo.setPeriodoInicial(date);
-					date = transformarStringDate(registro.substring(27,35));
-					headerArquivo.setperiodoFinal(date);
-					headerArquivo.setSequencia(Integer.parseInt(registro.substring(35,42)));
-					headerArquivo.setEmpresaAdquirinte(registro.substring(42,47));
-					headerArquivo.setTipoExtrato(Integer.parseInt(registro.substring(47,49)));
-					headerArquivo.setFiller(registro.substring(49,71));
-					headerArquivo.setVersaoLayout(registro.substring(70,75));
-					headerArquivo.setVersaoRelease(registro.substring(73,79));
-					headerArquivo.setUsoFuturo(registro.substring(78,530));
+					montaReader(registro);
+
 				}else if (tipoRegistro == 1){
-					//registro.substring() pega o valor inicial e o valor final-1
-					
-					//FALTA O USO FUTURO
 					Detalhe venda = montarVenda(registro);
 					
 					listaVendas.add(venda);
 				}else if(tipoRegistro == 9) {
 					
-					traillerArquivo.setTipoRegistro(Integer.parseInt(registro.substring(0,1)));
-					traillerArquivo.setTotalRegistro(Integer.parseInt(registro.substring(1,12)));
-					traillerArquivo.setUsoFuturo(registro.substring(12,530));
+					montaTrailler(registro);
+
 				}else if(tipoRegistro == -1) {
 					System.out.println("Erro na leitura");
 				}
 				
-				registro = bf.readLine();
+				registro = bufferedReaderArquivo.readLine();
 			}
 			
-			bf.close();
+			bufferedReaderArquivo.close();
 		}catch (IOException e) {
 			System.out.println(e);
 		}
@@ -73,57 +54,87 @@ public class Processamento {
 	}
 	
 	public Detalhe montarVenda(String registro) {
-		Detalhe venda = new Detalhe();
-		Date date;
-		Date hour;
-		venda.setTipoRegistro(Integer.parseInt(registro.substring(0,1)));
-		venda.setEstabelecimento(Integer.parseInt(registro.substring(1,11)));
-		date = transformarStringDate(registro.substring(11, 19));
-		venda.setDataInicialTransacao(date);
-		date = transformarStringDate(registro.substring(19, 27));
-		venda.setDataEvento(date);
-		hour = transformaStringHora(registro.substring(27, 33));
-		venda.setHoraEvento(hour); // vai ate o 33
-		venda.setTipoEvento(Integer.parseInt(registro.substring(33,35)));
-		venda.setTipoTransacao(Integer.parseInt(registro.substring(35,37)));
-		venda.setNumeroSerieLeitor(registro.substring(37,45));
-		venda.setCodTransacao(registro.substring(45,77));
-		venda.setCodPedido(registro.substring(77,97));
-		venda.setValorTotalTransacao(Long.parseLong(registro.substring(97,110)));
-		venda.setValorParcela(Long.parseLong(registro.substring(110,123)));
-		venda.setPagamento(registro.substring(123,124));
-		venda.setPlano(registro.substring(124,126));
-		venda.setParcela(registro.substring(126,128));
-		venda.setQtdParcela(Integer.parseInt(registro.substring(128,130)));
-		date = transformarStringDate(registro.substring(130,138));
-		venda.setDataPrevistaPagamento(date);
-		venda.setTaxaParcelamentoComprador(Long.parseLong(registro.substring(138,151)));
-		venda.setTarifaBoleto(Long.parseLong(registro.substring(151,164)));
-		venda.setValorOriginalTransacao(Long.parseLong(registro.substring(164,177)));
-		venda.setTaxaParcelamentoVendedor(Long.parseLong(registro.substring(177,190)));
-		venda.setTaxaIntermediacao(Long.parseLong(registro.substring(190,203)));
-		venda.setTarifaIntermediacao(Long.parseLong(registro.substring(203,216)));
-		venda.setTarifaBoletoVendedor(Long.parseLong(registro.substring(216,229)));
-		venda.setRepasseAplicacao(Long.parseLong(registro.substring(229,242)));
-		venda.setValorLiquidoTransacao(Long.parseLong(registro.substring(242,255)));
-		venda.setStatusPagamento(Integer.parseInt(registro.substring(255,257)));
-		venda.setFiller(registro.substring(257,259));
-		venda.setMeioPagamento(Integer.parseInt(registro.substring(259,261)));
-		venda.setInstituicaoFinanceira(registro.substring(261,291));
-		venda.setCanalEntrada(registro.substring(291,293));
-		venda.setLeitor(Integer.parseInt(registro.substring(293,295)));
-		venda.setMeioCaptura((Integer.parseInt(registro.substring(295,297))));
-		venda.setNumeroLogico(registro.substring(297,329));
-		venda.setNsu(registro.substring(329,340));
-		venda.setFiller(registro.substring(340,343));
-		venda.setCartaoBin(registro.substring(343,349));
-		venda.setCartaoHolder(registro.substring(349,353));
-		venda.setCodAutorizacao(registro.substring(353,359));
-		venda.setCodCV(registro.substring(359,391));
+		
+		Integer tipoDado = Integer.parseInt(registro.substring(0,1));
+		Integer estabelecimento = Integer.parseInt(registro.substring(1,11));
+		Date dataInicialTransacao = stringParaDate(registro.substring(11, 19));
+		Date dataEvento =  stringParaDate(registro.substring(19, 27));
+		Date horaEvento = stringParaHora(registro.substring(27, 33));
+		Integer tipoEvento = Integer.parseInt(registro.substring(33,35));
+		Integer tipoTransicao = Integer.parseInt(registro.substring(35,37));
+		String numeroSerieLeitor = registro.substring(37,45);
+		String codTransacao = registro.substring(45,77);
+		String codPedido = registro.substring(77,97);
+		Long valorTotalTransacao = Long.parseLong(registro.substring(97,110));
+		Long valorParcela = Long.parseLong(registro.substring(110,123));
+		String pagamento = registro.substring(123,124);
+		String plano = registro.substring(124,126);
+		String parcela = registro.substring(126,128);
+		Integer qtdParcela = Integer.parseInt(registro.substring(128,130));
+		Date dataPrevistaPagamento = stringParaDate(registro.substring(130,138));
+		Long taxaParcelamentoComprador = Long.parseLong(registro.substring(138,151));
+		Long tarifaBoleto = Long.parseLong(registro.substring(151,164));
+		Long valorOriginalTransacao = Long.parseLong(registro.substring(164,177));
+		Long taxaParcelamentoVendedor = Long.parseLong(registro.substring(177,190));
+		Long taxaIntermediacao = Long.parseLong(registro.substring(190,203));
+		Long tarifaIntermediacao = Long.parseLong(registro.substring(203,216));
+		Long tarifaBoletoVendedor = Long.parseLong(registro.substring(216,229));
+		Long repasseAplicacao = Long.parseLong(registro.substring(229,242));
+		Long valorLiquidoTransacao  = Long.parseLong(registro.substring(242,255));
+		Integer statusPagamento = Integer.parseInt(registro.substring(255,257));
+		String filler = registro.substring(257,259);
+		Integer meioPagamento = Integer.parseInt(registro.substring(259,261));
+		String instituicaoFinanceira = registro.substring(261,291);
+		String canalEntrada = registro.substring(291,293);
+		Integer leitor = Integer.parseInt(registro.substring(293,295));
+		Integer meioCaptura = Integer.parseInt(registro.substring(295,297));
+		String meioLogico = registro.substring(297,329);
+		String nsu = registro.substring(329,340);
+		filler = registro.substring(340,343);
+		String cartaoBin = registro.substring(343,349);
+		String cartaoHolder = registro.substring(349,353);
+		String codAutorizacao = registro.substring(353,359);
+		String codCV = registro.substring(359,391);
+
+		venda = new Detalhe(tipoDado, estabelecimento, dataInicialTransacao, dataEvento, horaEvento, tipoEvento, tipoTransicao, numeroSerieLeitor, codTransacao, 
+		codPedido,valorTotalTransacao, valorParcela, pagamento,plano ,parcela ,qtdParcela,dataPrevistaPagamento, taxaParcelamentoComprador, tarifaBoleto,valorOriginalTransacao, 
+		taxaParcelamentoVendedor, taxaIntermediacao, tarifaIntermediacao,tarifaBoletoVendedor, repasseAplicacao, valorLiquidoTransacao, statusPagamento,filler,
+		meioPagamento, instituicaoFinanceira, canalEntrada, leitor, meioCaptura, meioLogico, nsu, filler, cartaoBin, cartaoHolder, codAutorizacao, codCV  );
 		
 		return venda;
 	}
-	
+
+	public Header montaReader(String registro){
+		Integer tipoRegistro = Integer.parseInt(registro.substring(0,1));
+		Long estabelecimento = Long.parseLong(registro.substring(1,11));
+		Date dataProcessamento = stringParaDate(registro.substring(11,19));
+		Date periodoInicial = stringParaDate(registro.substring(19,27));
+		Date periodoFinal = stringParaDate(registro.substring(27,35));
+		Integer sequencia = Integer.parseInt(registro.substring(35,42));
+		String empresaAdquirinte = registro.substring(42,47);
+		Integer tipoExtrato = Integer.parseInt(registro.substring(47,49));
+		String filler = registro.substring(49,71);
+		String versaoLayout = registro.substring(70,75);
+		String versaoRelease = registro.substring(73,79);
+		String usoFuturo = registro.substring(78,530);
+
+		headerArquivo = new Header(tipoRegistro, estabelecimento, dataProcessamento, periodoInicial, periodoFinal, sequencia, empresaAdquirinte, tipoExtrato, filler, versaoLayout,
+		versaoRelease, usoFuturo) ;
+
+		return headerArquivo;
+	}
+
+	public Trailler montaTrailler(String registro){
+		Integer tipoRegistro = Integer.parseInt(registro.substring(0,1));
+		Integer totalRegistro = Integer.parseInt(registro.substring(1,12));
+		String usoFuturo = registro.substring(12,530);
+
+
+		traillerArquivo = new Trailler(tipoRegistro, totalRegistro, usoFuturo);
+		return traillerArquivo;
+	}
+
+
 	public Trailler getTrailler(){
 		return traillerArquivo;
 	}
@@ -132,31 +143,31 @@ public class Processamento {
 		return headerArquivo;
 	}
 
-	public Date transformarStringDate(String d){
-		String date = "";
-		date = date+d.charAt(0)+d.charAt(1)+d.charAt(2)+d.charAt(3)+"/"+d.charAt(4)+d.charAt(5)+"/"+d.charAt(6)+d.charAt(7);
-		Date dataN = null;
+	public Date stringParaDate(String stringData){
+		String data = "";
+		data = data+stringData.charAt(0)+stringData.charAt(1)+stringData.charAt(2)+stringData.charAt(3)+"/"+stringData.charAt(4)+stringData.charAt(5)+"/"+stringData.charAt(6)+stringData.charAt(7);
+		Date dataNova = null;
 		try {
 			SimpleDateFormat formato = new SimpleDateFormat("yyyy/MM/dd"); 
-			dataN = formato.parse(date);
-			return dataN;
+			dataNova = formato.parse(data);
+			return dataNova;
 		} catch (ParseException e) {
 			System.out.println("Erro em converter a data");
-			return dataN;
+			return dataNova;
 		}
 	}
 
-	public Date transformaStringHora(String d){
-		String date = "";
-		date = date+d.charAt(0)+d.charAt(1)+":"+d.charAt(2)+d.charAt(3)+":"+d.charAt(4)+d.charAt(5);
-		Date dataN = null;
+	public Date stringParaHora(String stringHora){
+		String hora = "";
+		hora = hora+stringHora.charAt(0)+stringHora.charAt(1)+":"+stringHora.charAt(2)+stringHora.charAt(3)+":"+stringHora.charAt(4)+stringHora.charAt(5);
+		Date horaNova = null;
 		try {
 			SimpleDateFormat formato = new SimpleDateFormat("HH:mm:ss"); 
-			dataN = formato.parse(date);
-			return dataN;
+			horaNova = formato.parse(hora);
+			return horaNova;
 		} catch (ParseException e) {
 			System.out.println("Erro em converter a data");
-			return dataN;
+			return horaNova;
 		}
 	}
 		
