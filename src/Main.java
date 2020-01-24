711865710766bd0ff4c45536e64167f00b1d978e
Pedro <p.paiva.pdo@gmail.com>
2020-01-24 00:18:32 -0300

    :recycle: alterado a classe processamento para adquirenteService

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,16 @@ import layout.Detalhe;
 import layout.Header;
 import layout.Trailler;
 import util.DetalheUtil;
+import service.AdquirenteService;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		ArrayList<Detalhe> listaVendas = new ArrayList<Detalhe>();
-		Processamento processamento = new Processamento("arquivos", listaVendas);
-		Trailler traillerArquivo = processamento.getTrailler();
-		Header headerArquivo = processamento.getHeader();
+		AdquirenteService adquirenteService = new AdquirenteService("arquivos", listaVendas);
+		Trailler traillerArquivo = adquirenteService.getTrailler();
+		Header headerArquivo = adquirenteService.getHeader();
 		DetalheUtil detalheUtil = new DetalheUtil(listaVendas, traillerArquivo, headerArquivo);
 	}
 
